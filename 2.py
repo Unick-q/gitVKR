07915e8e7eb78328fe914ce55b47b3ebae89f78f
Unick-q <76169705+Unick-q@users.noreply.github.com>
2020-12-17 17:33:42 +0200

    Delete 2.py

--- 2.py
+++
@@ -1,6 +0,0 @@
-age = input('Введите возраст человека в годах: ')
-
-if   age[1] in '1':    print(age, 'год')
-elif age in ['11','12','13','14','15','16','17','18','19']: print(age, 'лет')
-elif age[1] in '234':  print(age, 'года')
-else : print(age, 'лет')
